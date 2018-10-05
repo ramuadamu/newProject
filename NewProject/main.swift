0495b9a99c80b0de368873a1b76edc61e4286340
ramuadamu <ramuadamu@pursuit.org>
2018-10-05 15:59:13 -0400

    this is too much

---
+++ NewProject/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  NewProject
+//
+//  Created by Ramu on 10/5/18.
+//  Copyright © 2018 Ramu. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+let Ramu = ("firstName", "lastName", 29, drinker: false)
+print(Ramu.0)
